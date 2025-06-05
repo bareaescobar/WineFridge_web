dad02b298d53b4693f66303f344ff11794f5437b
Andriy Leliv <andrii.leliv@gmail.com>
2025-06-04 21:50:23 -0400

    Styles improvements & content load: - style fixes & cleaning - modal head margins & connect fridge figure - load content via handlebars

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,16 @@ import path from 'path'
 import { defineConfig } from 'vite'
 import fs from 'fs'
 import includeHtml from 'vite-plugin-include-html'
-import contentPlugin from './vite-plugins/content-plugin'
+import handlebars from 'vite-plugin-handlebars'
+import context from './content/main.json'
 
 function getHtmlEntryFiles(srcDir) {
     const entry = {}
-
     function traverseDir(currentDir) {
         const files = fs.readdirSync(currentDir)
-
         files.forEach((file) => {
             const filePath = path.join(currentDir, file)
             const isDirectory = fs.statSync(filePath).isDirectory()
-
             if (isDirectory) {
                 traverseDir(filePath)
             } else if (path.extname(file) === '.html') {
@@ -22,23 +20,26 @@ function getHtmlEntryFiles(srcDir) {
             }
         })
     }
-
     traverseDir(srcDir)
-
     return entry
 }
 
 export default defineConfig({
     root: './src',
     base: './',
-
-    plugins: [includeHtml(), contentPlugin()],
-
+    plugins: [
+        includeHtml(),
+        handlebars({
+            context,
+            helpers: {
+                findSelectedFridge: (fridges) => fridges.find((fridge) => fridge.selected),
+            },
+        }),
+    ],
     server: {
         open: './src/index.html',
         host: true,
     },
-
     css: {
         devSourcemap: true,
         preprocessorOptions: {
@@ -47,19 +48,16 @@ export default defineConfig({
             },
         },
     },
-
     build: {
         outDir: '../dist',
         assetsDir: './',
         assetsInlineLimit: 4096,
         emptyOutDir: true,
         target: 'es2015',
-
         rollupOptions: {
             input: getHtmlEntryFiles('src'),
             output: {
                 manualChunks: undefined,
-
                 assetFileNames: ({ name }) => {
                     if (/\.(gif|jpe?g|png|svg)$/.test(name ?? '')) {
                         return `[name][extname]`
